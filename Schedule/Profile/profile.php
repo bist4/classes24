647347f67c9c9398833b7c03a7c7931c7d226ad0
bist4 <bsit9200@gmail.com>
2024-03-19 18:42:48 +0800

    remove the profile pic at profile admin

--- Schedule/Profile/profile.php
+++ Schedule/Profile/profile.php
@@ -239,7 +239,7 @@ include('session_out.php');
                     <div class="row">
                         <div class="col-xl-4">
                             <!-- Profile picture card-->
-                            <div class="card mb-4 mb-xl-0">
+                            <!-- <div class="card mb-4 mb-xl-0">
                                 <div class="card-header">Profile Picture</div>
                                 <div class="card-body text-center">
                                     <!-- Profile picture image-->
@@ -249,7 +249,7 @@ include('session_out.php');
                                     <!-- Profile picture upload button-->
                                     <button style="display: none;" class="btn btn-primary" type="button">Upload new image</button>
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                         <div class="col-xl-8">
                             <!-- Account details card-->

2655e2c28cb6f77cb4a5c999005d7024032fd1a1
bist4 <bsit9200@gmail.com>
2024-03-27 15:08:35 +0800

    changhe the code

--- Schedule/records/DataGet/get_instructor.php
+++ Schedule/records/DataGet/get_instructor.php
@@ -9,7 +9,7 @@ if (isset($_POST['departmentID'])) {
         INNER JOIN userinfo usi ON i.UserInfoID = usi.UserInfoID
         LEFT JOIN instructorspecializations isp ON isp.InstructorID = i.InstructorID
         
-        WHERE i.is_$departmentID = 1 AND i.Active = 11";
+        WHERE i.is_$departmentID = 1 AND i.Active = 1";
 
     $result = $conn->query($sql);
 

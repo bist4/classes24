37ec5639ee06625dcdf166adcc9a47ba8a42183e
bist4 <bsit9200@gmail.com>
2024-03-27 16:32:48 +0800

    change the code

--- Schedule/utilities/EditUser/edit_user.php
+++ Schedule/utilities/EditUser/edit_user.php
@@ -515,49 +515,7 @@ include('../session_out.php');
 
     </script>
 
-    <!-- <script>
-        $(document).ready(function () {
-        $('#updateBtn').on('click', function () {
-
-            var formData = $('#updateForm').serialize(); // Serialize the form data
-
-      
-            $.ajax({
-                type: 'POST',
-                url: '../UpdateUser/update_user.php',
-                data: formData,
-                success: function (response) {
-                    response = JSON.parse(response);
-                    if (response.success) {
-                        Swal.fire({
-                            title: "Success!",
-                            text: response.success,
-                            icon: "success",
-                        }).then(function () {
-                            window.location.href = '../accounts.php';
-                        });
-                    } else if (response.error) {
-                        Swal.fire({
-                            title: "Warning!",
-                            text: response.error,
-                            icon: "warning",
-                        });
-                    }
-                },
-                error: function (xhr, status, error) {
-                    Swal.fire({
-                        title: 'Error',
-                        text: 'Failed to update user information!',
-                        icon: 'error',
-                        confirmButtonText: 'OK'
-                    });
-                    console.error(xhr.responseText);
-                }
-            });
-        });
-        });
-    </script> -->
-
+ 
     <script>
     // JavaScript for form validation
         (function () {
@@ -582,21 +540,22 @@ include('../session_out.php');
     </script>
 
 
+ 
+
+  
 <!-- <script>
     $(document).ready(function(){
         $('#updateBtn').click(function(){
 
             var fields = document.querySelectorAll("input");
             var error = false;
-            
-        
 
             fields.forEach(function(field) {
                 var trimmedValue = field.value.trim();
-                if (field.id !== "Mname" && trimmedValue === ""){
+                if (field.name !== "Mname" && trimmedValue === ""){
                     error = true;
                     field.classList.add("is-invalid");
-                }else if (field.id !== "Mname" && /^\s/.test(field.value)){
+                } else if (field.name !== "Mname" && /^\s/.test(field.value)){
                     error = true;
                     field.classList.add("is-invalid");
                     Swal.fire({
@@ -604,13 +563,12 @@ include('../session_out.php');
                         title: 'Error',
                         text: 'Space before letters are not allowed',
                     });
-                }
-                else {
+                } else {
                     field.classList.remove("is-invalid");
                 }
 
                 // Validate contact number
-                if (field.id === "ContactNumber" && !/^\d+$/.test(field.value)) {
+                if (field.name === "Cnumber" && !/^\d+$/.test(field.value)) {
                     error = true;
                     field.classList.add("is-invalid");
                     Swal.fire({
@@ -625,170 +583,77 @@ include('../session_out.php');
                 return false;
             }
 
-            if($('.needs-validation')[0].checkValidity()){
-                var loading = Swal.fire({
-                    title: 'Please wait',
-                    html: 'Updating your data...',
-                    allowOutsideClick: false,
-                    onBeforeOpen: () => {
-                        Swal.showLoading();
-                    }
-                });
-
-                
-
-                $.ajax({
-                    type: 'POST',
-                    url: '../UpdateUser/update_user.php', 
-                    data: $('.needs-validation').serialize(),
-                    dataType: 'json', // Specify data type as JSON
-                    success: function(response){
-                        loading.close();
-                        if(response.success){
-                            Swal.fire({
-                                icon: 'success',
-                                title: 'Success!',
-                                text: response.success,
-                                onClose: () => {
-                                    // Reload the page
-                                    window.location.reload();
-                                }
-                            }).then(function() {
-                                var subid = "<?php echo $_GET['subid']; ?>";
-                                window.location.href = 'edit_user.php?subid=' + subid;
-                            });
-                        } else if(response.error){
-                            Swal.fire({
-                                icon: 'warning',
-                                title: 'Warning',
-                                text: response.error,
-                            });
-                        }
-                    },
-                    error: function(xhr, status, error){
-                        // Close loading animation
-                        loading.close();
-
-                        // Show error message
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Error!',
-                            text: 'An error occurred while submitting your data. Please try again later.'
-                        });
-                    }
-                });
-            } else {
-                // If the form is invalid, show a sweet alert message
-                Swal.fire({
-                    icon: 'warning',
-                    title: 'Warning',
-                    text: 'Please fill in all required fields.'
-                });
-            }
-        });
-    });
-</script> -->
-
-
-<!-- <script>
-   $(document).ready(function(){
-    $('#updateBtn').click(function(){
-
-        var fields = document.querySelectorAll("input");
-        var error = false;
-
-        fields.forEach(function(field) {
-            var trimmedValue = field.value.trim();
-            if (field.name !== "Mname" && trimmedValue === ""){
-                error = true;
-                field.classList.add("is-invalid");
-            } else if (field.name !== "Mname" && /^\s/.test(field.value)){
-                error = true;
-                field.classList.add("is-invalid");
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Space before letters are not allowed',
-                });
-            } else {
-                field.classList.remove("is-invalid");
-            }
-
-            // Validate contact number
-            if (field.name === "Cnumber" && !/^\d+$/.test(field.value)) {
-                error = true;
-                field.classList.add("is-invalid");
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Contact number must contain only numbers.',
-                });
-            }
-        });
-
-        if (error) {
-            return false;
-        }
-
-        if($('.needs-validation')[0].checkValidity()){
-            var loading = Swal.fire({
-                title: 'Please wait',
-                html: 'Updating your data...',
-                allowOutsideClick: false,
-                showConfirmButton: false,
-                onBeforeOpen: () => {
-                    Swal.showLoading();
-                }
-            });
-
-            $.ajax({
-                type: 'POST',
-                url: '../UpdateUser/update_user.php', 
-                data: $('.needs-validation').serialize(),
-                dataType: 'json', // Specify data type as JSON
-                success: function(response){
-                    loading.close();
-                    if(response.success){
-                        Swal.fire({
-                            icon: 'success',
-                            title: 'Success!',
-                            text: response.message,
+            // Show confirmation message before proceeding with the update
+            Swal.fire({
+                title: 'Are you sure?',
+                text: 'Do you want to update the user data?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, update it!',
+                cancelButtonText: 'Cancel'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Proceed with the update operation
+                    if($('.needs-validation')[0].checkValidity()){
+                        var loading = Swal.fire({
+                            title: 'Please wait',
+                            html: 'Updating your data...',
+                            allowOutsideClick: false,
                             showConfirmButton: false,
-                        }).then(function() {
-                            var subid = "<?php echo $_GET['subid']; ?>";
-                            window.location.href = 'edit_user.php?subid=' + subid;
+                            onBeforeOpen: () => {
+                                Swal.showLoading();
+                            }
                         });
-                    } else if(response.error){
+
+                        $.ajax({
+                            type: 'POST',
+                            url: '../UpdateUser/update_user.php', 
+                            data: $('.needs-validation').serialize(),
+                            dataType: 'json', // Specify data type as JSON
+                            success: function(response){
+                                loading.close();
+                                if(response.success){
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Success!',
+                                        text: response.message,
+                                        showConfirmButton: false,
+                                    }).then(function() {
+                                        var subid = "<?php echo $_GET['subid']; ?>";
+                                        window.location.href = 'edit_user.php?subid=' + subid;
+                                    });
+                                } else if(response.error){
+                                    Swal.fire({
+                                        icon: 'warning',
+                                        title: 'Warning',
+                                        text: response.message,
+                                    });
+                                }
+                            },
+                            error: function(xhr, status, error){
+                                // Close loading animation
+                                loading.close();
+
+                                // Show error message
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Error!',
+                                    text: 'An error occurred while submitting your data. Please try again later.'
+                                });
+                            }
+                        });
+                    } else {
+                        // If the form is invalid, show a sweet alert message
                         Swal.fire({
                             icon: 'warning',
                             title: 'Warning',
-                            text: response.message,
+                            text: 'Please fill in all required fields.'
                         });
                     }
-                },
-                error: function(xhr, status, error){
-                    // Close loading animation
-                    loading.close();
-
-                    // Show error message
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error!',
-                        text: 'An error occurred while submitting your data. Please try again later.'
-                    });
                 }
             });
-        } else {
-            // If the form is invalid, show a sweet alert message
-            Swal.fire({
-                icon: 'warning',
-                title: 'Warning',
-                text: 'Please fill in all required fields.'
-            });
-        }
+        });
     });
-});
-
 </script> -->
 <script>
     $(document).ready(function(){

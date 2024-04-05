9a2c2a96c731716d538c6ce8bb62532be8d07cd6
bist4 <bsit9200@gmail.com>
2024-04-05 19:11:45 +0800

    change the clde

--- Schedule/records/EditData/edit_section.php
+++ Schedule/records/EditData/edit_section.php
@@ -593,32 +593,39 @@ include('session_out.php');
             }
         }
 
-        // Function to validate form fields
         function validateFormFields() {
-            var fields = document.querySelectorAll("input");
-            fields.forEach(function(field) {
-                var trimmedValue = field.value.trim();
-                if ((field.id === "subDesc" ) && !/^[a-zA-Z]*$/.test(trimmedValue)) {
-                    showValidationMessage(field, 'Only letters are allowed.');
-                } else if ((field.id === "SectionName" || field.id === "subDesc") && !trimmedValue) {
-                    showValidationMessage(field, 'This field cannot be empty.');
-                } else if (field.id === "SubjectCode") {
-                    if (trimmedValue !== "" && !/^[a-zA-Z]*$/.test(trimmedValue)) {
-                        showValidationMessage(field, 'Only letters are allowed.');
-                    } else if (/^\s/.test(field.value)) {
-                        showValidationMessage(field, 'Spaces before letters are not allowed.');
-                    } else {
-                        hideValidationMessage(field);
-                    }
-                } else if (field.id !== "SectionNo" && /^\s/.test(field.value)) {
-                    showValidationMessage(field, 'Spaces before letters are not allowed.');
-                } else if (field.id === "SectionNo" && !/^\d*$/.test(field.value)) {
-                    showValidationMessage(field, 'Minutes Per Week number must contain only numbers.');
-                } else {
-                    hideValidationMessage(field);
-                }
-            });
+    var fields = document.querySelectorAll("input");
+    var hasErrors = false; // Flag to track if there are any validation errors
+    fields.forEach(function(field) {
+        var trimmedValue = field.value.trim();
+        if (field.id === "SectionNo" && !/^\d*$/.test(trimmedValue)) {
+            showValidationMessage(field, 'Section Number must contain only numbers.');
+            hasErrors = true; // Set flag to true if there is an error
+        } else if ((field.id === "SectionName" || field.id === "subDesc") && !trimmedValue) {
+            showValidationMessage(field, 'This field cannot be empty.');
+            hasErrors = true; // Set flag to true if there is an error
+        } else if ((field.id === "subDesc" || field.id === "SubjectCode") && !/^[a-zA-Z]*$/.test(trimmedValue)) {
+            showValidationMessage(field, 'Only letters are allowed.');
+            hasErrors = true; // Set flag to true if there is an error
+        } else if (field.id === "SubjectCode" && /^\s/.test(field.value)) {
+            showValidationMessage(field, 'Spaces before letters are not allowed.');
+            hasErrors = true; // Set flag to true if there is an error
+        } else if (field.id !== "SectionNo" && /^\s/.test(field.value)) {
+            showValidationMessage(field, 'Spaces before letters are not allowed.');
+            hasErrors = true; // Set flag to true if there is an error
+        } else {
+            hideValidationMessage(field);
         }
+    });
+
+    // If errors were found, disable the update button
+    if (hasErrors) {
+        $('#updateBtn').prop('disabled', true);
+    } else {
+        $('#updateBtn').prop('disabled', false);
+    }
+}
+
 
 
         // Event listener for input fields to validate while typing

a6f365a17875c02b6969dc1cdfc30161cf63ca0c
bist4 <bsit9200@gmail.com>
2024-04-11 17:06:33 +0800

    side bar

--- Schedule/Profile/profile.php
+++ Schedule/Profile/profile.php
@@ -99,20 +99,21 @@ include('session_out.php');
                 
             </div>
 
-            <!-- Nav Item - File Maimntenance Menu -->
-            <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo"
+           <!-- Nav Item - File Maimntenance Menu -->
+           <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-folder"></i>
                     <span>File Maintenance</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="../filemaintenance/file_strand.php">Strand</a>
-                        <a class="collapse-item" href="../filemaintenance/file_subject.php">Subject</a>
-                        <a class="collapse-item" href="../filemaintenance/file_section.php">Section</a>
-                        <a class="collapse-item" href="../filemaintenance/file_instructor.php">Instructor</a>
-                        <a class="collapse-item" href="../filemaintenance/file_room.php">Room</a>
+                        <a class="collapse-item" href="filemaintenance/file_strand.php">Strand</a>
+                        <a class="collapse-item" href="filemaintenance/file_subject.php">Subject</a>
+                        <!-- <a class="collapse-item" href="filemaintenance/file_instructor.php">Instructor</a> -->
+                        <a class="collapse-item" href="filemaintenance/file_section.php">Class Section</a>
+                        <a class="collapse-item" href="filemaintenance/file_room.php">Room</a>
+                        <a class="collapse-item" href="filemaintenance/timeAvail.php">Instructor Availability</a>
                     </div>
                 </div>
             </li>
@@ -122,18 +123,21 @@ include('session_out.php');
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseView"
                     aria-expanded="true" aria-controls="collapseView">
                     <i class="fas fa-fw fa-eye"></i>
-                    <span>View Schedule</span>
+                    <span>View Records</span>
                 </a>
                 <div id="collapseView" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="">Class</a>
-                        <a class="collapse-item" href="">Instructor</a>
-                        <a class="collapse-item" href="">Room</a>
+                        <a class="collapse-item" href="../view_strand.php">Strand</a>
+                        <a class="collapse-item" href="../view_subject.php">Subject</a>
+                        <a class="collapse-item" href="../view_instructor.php">Instructor</a>
+                        <a class="collapse-item" href="../view_section.php">Class Section</a>
+                        <a class="collapse-item" href="../view_room.php">Room</a>
+                        <a class="collapse-item" href="../view_timeAvail.php">Instructor Availability</a>
                     </div>
                 </div>
             </li>
 
-
+           
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
@@ -144,11 +148,13 @@ include('session_out.php');
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        
-                        <a class="collapse-item" href="../utilities/accounts.php">Accounts</a>
-                        <a class="collapse-item" href="../utilities/archive.php">Archive</a>
-                        <a class="collapse-item" href="../utilities/backup.php">Back Up</a>
-                        <a class="collapse-item" href="../utilities/logs.php">Logs</a>
+                         
+                        <a class="collapse-item" href="utilities/accounts.php">Account Management</a>
+                        <a class="collapse-item" href="utilities/archive.php">Archive</a>
+                        <!-- <a class="collapse-item" href="utilities/backup.php">Back Up</a> -->
+                        <a class="collapse-item" href="utilities/logs.php">Activity History</a>
+                        <a class="collapse-item" href="utilities/trash.php">Trash</a>
+
                     </div>
                 </div>
             </li>
